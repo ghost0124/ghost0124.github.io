f22e2444658cee2cfe795562c74dec400f1d1bcc
nemjoadam@gmail.com <nemjoadam@gmail.com>
2023-07-27 22:24:36 +0200

    - prepared entites page - only need to fill them all out

--- src/pages/entities.js
+++ src/pages/entities.js
@@ -1,20 +1,36 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import * as styles from "../styles/entities.module.css"
-import ListGroup from 'react-bootstrap/ListGroup';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
 
 export default function Entities() {
     return (
         <Layout>
-            <div>
-                <h1 className={styles.header}>Cthulhu Mythos Entities</h1>
-                <ListGroup horizontal>
-                    <ListGroup.Item>Cthulhu</ListGroup.Item>
-                    <ListGroup.Item>Cthulhu</ListGroup.Item>
-                    <ListGroup.Item>Cthulhu</ListGroup.Item>
-                    <ListGroup.Item>Cthulhu</ListGroup.Item>
-                </ListGroup>
-            </div>
+            <h1 className={styles.header}>Cthulhu Mythos Entities</h1>
+            <Tabs defaultIndex={1}>
+                <TabList>
+                    <Tab>The Great Old Ones</Tab>
+                    <Tab>The Great Ones</Tab>
+                    <Tab>The Outer Gods</Tab>
+                    <Tab>The Elder Gods</Tab>
+                    <Tab>The Great Ones</Tab>
+                </TabList>
+                <TabPanel>
+                    <Tabs defaultIndex={1}>
+                        <TabList>
+                            <Tab>Cthulhu</Tab>  
+                            <Tab>Yogsototh</Tab>
+                        </TabList>
+                        <TabPanel>
+                            Cthulhu entity
+                        </TabPanel>
+                        <TabPanel>
+                            Yogsototh entity
+                        </TabPanel>
+                    </Tabs>
+                </TabPanel>
+            </Tabs>
         </Layout>
     )
 }
