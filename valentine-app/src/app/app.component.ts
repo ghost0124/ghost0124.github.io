ae7680dc8fef2df87d2b0c2f9c86f8e180d47758
nemjoadam@gmail.com <nemjoadam@gmail.com>
2026-02-10 19:33:36 +0100

    asd

--- valentine-app/src/app/app.component.ts
+++ valentine-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,37 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'valentine-app';
+  @ViewChild('btnNo') noBtn!: ElementRef<HTMLAnchorElement>;
+  loveVisible = false;
+  originalPos: any; 
+  
+  ngAfterViewInit() {
+    this.originalPos = this.noBtn.nativeElement.style.transform || '';
+  }
+
+  runAway(): void {
+    if (!this.noBtn?.nativeElement) return;
+
+    const x = Math.random() * 400;
+    const y = Math.random() * 400;
+
+    this.noBtn.nativeElement.style.transform = `translate(${x}px, ${y}px)`;
+  }
+
+  refreshNo(): void {
+    if (!this.noBtn?.nativeElement) return;
+
+    const x = Math.random() * 400;
+    const y = Math.random() * 400;
+
+    this.noBtn.nativeElement.style.transform = this.originalPos
+  }
+  
+  showLoveSection(): void {
+    this.loveVisible = true;
+    // Optional: scroll smoothly
+    setTimeout(() => {
+      document.getElementById('love')?.scrollIntoView({ behavior: 'smooth' });
+    }, 50);
+  }
 }
