0e6433d6ce5fbb2cd2cdff308445d70f7b5ab878
nemjoadam@gmail.com <nemjoadam@gmail.com>
2026-03-05 13:56:57 +0100

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/womans-day',
   allowedHosts: [],
   supportedLocales: {
   "en-US": ""
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/womans-day/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/womans-day"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 636, hash: '46d12260392083a758f635f35b92e9f09c2e558cb89a5ec40ba6cc627f8b6586', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 947, hash: '9b07e2f775f85263c7c5dbbcc6b2521aa096f03829b9af6bceb13ea89d604905', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 4737, hash: '60c8fc95412098bb253c7b99363924dfd9f89c62d55dc07a0e7cdc1c14de206c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 647, hash: 'b578fc017ecea0cfeadb0b3733bb7143de6c7c7d3e35257e188d5186023b9445', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 958, hash: '868e1f76682d4f559ff3b76fa98dbe74a6c171daa9cf5186bbc5535a2707fd1d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 4748, hash: '7a89464b3d8fdf8389a9b530dc056df0a56810c16cceb207e57fb7f84b2e3ad5', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-IRTP4LPO.css': {size: 77, hash: 'Dp4wwC6I+14', text: () => import('./assets-chunks/styles-IRTP4LPO_css.mjs').then(m => m.default)}
   },
 };
